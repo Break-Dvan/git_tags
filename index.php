428cc24e2b283711d811d9fa892b92d10db80057
Dick BkD <bkd@hoornbeeck.nl>
2026-03-10 16:21:17 +0100

    index.php extra text with h3

--- index.php
+++ index.php
@@ -4,7 +4,11 @@ include 'include/header.php';
 <h1>Uitleg git tags</h1>
 <p>Tags moeten handig zijn om terug te kunnen naar een vast punt. <br>Tags zijn niet bij te werken, maar statisch.<br>
 Dat is anders dus dan een branche.</p>
-
+<h3>Stap 1</h3>
+<h3>Stap 2</h3>
+<h3>Stap 3</h3>
+<h3>Stap 4</h3>
+Extra stappen inhoudelijk toegevoegd. Dit betreft dan Stap_3. (tag: Stap_3)
 
 <?php
 include 'include/footer.php';
