86b75ed50ce77574453c7ce51a5056fd2639c0ce
Dick BkD <bkd@hoornbeeck.nl>
2026-03-10 16:14:43 +0100

    Uitbreiding index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 </head>
 <body>
 <h1>Uitleg git tags</h1>
+<p>Tags moeten handig zijn om terug te kunnen naar een vast punt. <br>Tags zijn niet bij te werken, maar statisch.<br>
+Dat is anders dus dan een branche.</p>
 </body>
 </html>
 
