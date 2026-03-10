61f071a5741471ef46955785c770982c0c53f31b
Dick BkD <bkd@hoornbeeck.nl>
2026-03-10 16:18:06 +0100

    Add header.php and footer.php: stuctured programming!

---
+++ include/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Git Tags</title>
+</head>
+<body>
+<?php
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Git Tags</title>
-</head>
-<body>
+<?php
+include 'include/header.php';
+?>
 <h1>Uitleg git tags</h1>
 <p>Tags moeten handig zijn om terug te kunnen naar een vast punt. <br>Tags zijn niet bij te werken, maar statisch.<br>
 Dat is anders dus dan een branche.</p>
-</body>
-</html>
+
 
 <?php
-
+include 'include/footer.php';
 ?>
